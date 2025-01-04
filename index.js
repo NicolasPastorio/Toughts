0adae347d447ccfdae8729a3e6b8aa410a366230
NicolasPastorio <npastoriodemoura@gmail.com>
2025-01-04 10:16:08 -0300

    Middleware de verificação de autenticação

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.use('toughts', toughtsRoutes);
+app.use('/toughts', toughtsRoutes);
 app.use('/', authRoutes);
 
 app.use('/', ToughtController.showToughts);
